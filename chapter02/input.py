3fca584b6bc30b151231122a7bddd74808f06281
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-05-24 21:15:34 +0530

    sd;lk

---
+++ chapter02/input.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number a "))
+
+b = int(input("Enter number b "))
+
+print("Number 1 a is : ", a)
+print("Number 1 b is : ", b)
+
+
+print("Sume is : ", a+b)
---
+++ chapter03/string.py
@@ -0,0 +1,38 @@
+# name = "Sarfraz Qadri"
+
+# nameShort = name[0:3]
+# nameShort = name[0:-1]
+# nameShort = name[-4:-1]
+# nameShort = name[:]
+# nameShort = name.__len__()
+# nameShort = name[0:10:3]
+# nameShort = len(name)
+# nameShort = name.capitalize()
+# nameShort = name.capitalize()
+
+# print(nameShort)
+
+# print(name.endswith("Qadri"))    
+# print(name.startswith("Sar"))    
+
+
+
+# a = "you are a good boy"
+
+# num = a.count('o')
+
+# print(num)
+
+
+# a = "Harry is a goood body\nbut not a bad \"boy\""
+# print(a)
+
+# name = input("Enter your name : ")
+# print(f"Good night {name}")
+
+
+letter =  '''Dear <|Name|>,
+your are selected!
+<|Date|>'''
+
+print(letter.replace("<|Name|>", "Sarfraz").replace("<|Date|>", "24 sep 2025"))
